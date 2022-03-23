9f4628d9c0fe34665160aa2e9d229da24407bc99
yaosiqi <504967290@qq.com>
2022-03-23 22:05:12 +0800

    first commit

---
+++ src/main/java/com/integr/CamundaApp.java
@@ -0,0 +1,12 @@
+package com.integr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaApp {
+    public static void main(String[] args) {
+
+        SpringApplication.run(CamundaApp.class, args);
+    }
+}
